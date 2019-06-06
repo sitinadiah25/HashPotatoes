81bede6ca902f3ef728b7118c3474735a8523b19
Janice <Janice.tohy2@gmail.com>
2019-06-06 17:51:36 +0800

    Made changes to profile fragment to facilitate change from profile activity

--- HashPotatoesv2.0/app/src/main/java/com/example/hashpotatoesv20/Profile/ProfileFragment.java
+++ HashPotatoesv2.0/app/src/main/java/com/example/hashpotatoesv20/Profile/ProfileFragment.java
@@ -1,23 +1,92 @@
 package com.example.hashpotatoesv20.Profile;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.hashpotatoesv20.R;
+import com.example.hashpotatoesv20.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
 
     private static final String TAG = "ProfileFragment";
+    private static final int ACTIVITY_NUM = 3;
+
+    private TextView mPosts, mHashtags, mDisplayName, mYear, mMajor;
+    private ProgressBar mProgressBar;
+    private CircleImageView mProfilePhoto;
+    private Toolbar toolbar;
+    private ImageView profileMenu;
+    private BottomNavigationViewEx bottomNavigationView;
+
+    private Context mContext;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_profile, container,false);
+        mDisplayName = (TextView) view.findViewById(R.id.display_name);
+        mYear = (TextView) view.findViewById(R.id.profile_year);
+        mMajor = (TextView) view.findViewById(R.id.profile_major);
+        mProfilePhoto = (CircleImageView) view.findViewById(R.id.profilePhoto);
+        mPosts = (TextView) view.findViewById(R.id.tvPost);
+        mHashtags = (TextView) view.findViewById(R.id.tvHash);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.profileProgressBar);
+        toolbar = (Toolbar) view.findViewById(R.id.profileToolBar);
+        profileMenu = (ImageView) view.findViewById(R.id.profileMenu);
+        bottomNavigationView = (BottomNavigationViewEx) view.findViewById(R.id.bottomNavViewBar);
+        mContext = getActivity();
+
+        setupBottomNavigationView();
+        setupToolbar();
+
+        Log.d(TAG, "onCreateView: Started.");
 
         return view;
     }
+
+    /**
+     * Responsible for setting up the profile Toolbar
+     */
+    private void setupToolbar() {
+        ((ProfileActivity) getActivity()).setSupportActionBar(toolbar);
+
+        profileMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to account settings.");
+                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up bottom navigation view");
+
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+
 }
