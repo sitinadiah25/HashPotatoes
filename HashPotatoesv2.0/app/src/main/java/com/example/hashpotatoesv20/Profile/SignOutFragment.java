d5b0c6d42fec47d6d4034fd1ff4dcf5406a28d91
Janice <Janice.tohy2@gmail.com>
2019-06-06 16:26:53 +0800

    Added the signout feature

--- HashPotatoesv2.0/app/src/main/java/com/example/hashpotatoesv20/Profile/SignOutFragment.java
+++ HashPotatoesv2.0/app/src/main/java/com/example/hashpotatoesv20/Profile/SignOutFragment.java
@@ -1,23 +1,104 @@
 package com.example.hashpotatoesv20.Profile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.example.hashpotatoesv20.Login.LoginActivity;
 import com.example.hashpotatoesv20.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignOutFragment extends Fragment {
 
     private static final String TAG = "SignOutFragment";
 
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private ProgressBar mProgressbar;
+    private TextView tvSignout,tvSigningout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signout, container,false);
+        tvSignout = (TextView) view.findViewById(R.id.tvConfirmSignout);
+        mProgressbar = (ProgressBar) view.findViewById(R.id.progressBar);
+        tvSigningout = (TextView) view.findViewById(R.id.tvSigningout) ;
+        Button btnConfirmSignout = (Button) view.findViewById(R.id.btnConfirmSignout);
+
+        mProgressbar.setVisibility(View.GONE);
+        tvSigningout.setVisibility(View.GONE);
+
+        btnConfirmSignout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d(TAG, "onClick: attempting to sign out.");
+                mProgressbar.setVisibility(View.VISIBLE);
+                tvSigningout.setVisibility(View.VISIBLE);
+
+                mAuth.signOut();
+                getActivity().finish();
+            }
+        });
 
         return view;
+    }
+
+    /*
+    ---------------------------------------firebase------------------------------------------------
+     */
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    //User is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in:" + user.getUid());
+                }
+                else {
+                    //User is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed out");
+
+                    Log.d(TAG, "onAuthStateChanged: navigating back to login screen.");
+                    Intent intent = new Intent(getActivity(),LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
