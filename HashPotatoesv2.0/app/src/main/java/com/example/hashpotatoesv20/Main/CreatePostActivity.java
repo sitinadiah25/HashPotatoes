b9d5d033aed2a170cfd8672b94804dbe109d7f4a
nadiah25 <sitinadiah333@gmail.com>
2019-06-08 01:43:54 +0800

    CreatePostActivity, layout

---
+++ HashPotatoesv2.0/app/src/main/java/com/example/hashpotatoesv20/Main/CreatePostActivity.java
@@ -0,0 +1,216 @@
+package com.example.hashpotatoesv20.Main;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.hashpotatoesv20.R;
+import com.example.hashpotatoesv20.Utils.FirebaseMethods;
+import com.example.hashpotatoesv20.Utils.Permissions;
+import com.example.hashpotatoesv20.Utils.SectionsStatePagerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class CreatePostActivity extends AppCompatActivity {
+    private static final String TAG = "CreatePostActivity";
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseMethods mFirebaseMethods;
+
+    //widgets
+    private EditText mDiscussion, mTag;
+    private SwitchCompat mAnonymity;
+
+    //constants
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
+
+    private ViewPager mViewPager;
+
+    private Context mContext = CreatePostActivity.this;
+
+    @Nullable
+    //@Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_createpost, container,false);
+
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createpost);
+        Log.d(TAG, "onCreate: CreatePost Activity started.");
+        //mFirebaseMethods = new FirebaseMethods(CreatePostActivity.this);
+        mDiscussion = (EditText) findViewById(R.id.discussion);
+
+        Button cancel = (Button) findViewById(R.id.btn_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing activity, to back to previous screen");
+                finish();
+            }
+        });
+
+        Button share = (Button) findViewById(R.id.btn_share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: sharing post.");
+                //upload post to firebase
+                Toast.makeText(CreatePostActivity.this, "Attempting to upload post", Toast.LENGTH_SHORT).show();
+                //String discussion = mDiscussion.getText().toString();
+                //String tag = mTag.getText().toString();
+
+                //start here!
+                //mFirebaseMethods.uploadPost(discussion, tag, anonymity);
+            }
+        });
+
+        /*
+        if (checkPermissionsArray(Permissions.PERMISSIONS)) {
+
+        }
+        else {
+            verifyPermissions(Permissions.PERMISSIONS);
+        }*/
+    }
+
+    /**
+     * Verify all the permissions passed to the array
+     * @param permissions
+     */
+    public void verifyPermissions(String[] permissions) {
+        Log.d(TAG, "verifyPermissions: verifying permissions.");
+        ActivityCompat.requestPermissions(
+                CreatePostActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+
+    /**
+     * Check an array of permissions
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissionsArray(String[] permissions) {
+        Log.d(TAG, "checkPermissionsArray: checking permissions array.");
+
+        for (int i = 0; i < permissions.length; i++) {
+            String check = permissions[i];
+            if (!checkPermissions(check)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check single permission if it has been verified
+     * @param permission
+     * @return
+     */
+    public boolean checkPermissions(String permission) {
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(CreatePostActivity.this, permission);
+
+        if (permissionRequest != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "checkPermissions: Permission is not granted for: " + permission);
+            return false;
+        }
+        Log.d(TAG, "checkPermissions: Permission is granted for: " + permission);
+        return true;
+    }
+
+    private void someMethod() {
+        //Properties to Posts (discussion, date, tag, anonymity)
+
+    }
+
+    /*
+    ---------------------------------------firebase------------------------------------------------
+    */
+
+    /**
+     * Setup the firebase auth object
+
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    //User is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in:" + user.getUid());
+                }
+                else {
+                    //User is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed out");
+                }
+            }
+        };
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //retrieve user information from the database
+
+                //retrieve posts for the user in question
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    **/
+}
--- HashPotatoesv2.0/app/src/main/java/com/example/hashpotatoesv20/Main/MainActivity.java
+++ HashPotatoesv2.0/app/src/main/java/com/example/hashpotatoesv20/Main/MainActivity.java
@@ -3,23 +3,36 @@ package com.example.hashpotatoesv20.Main;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.hashpotatoesv20.Login.LoginActivity;
+import com.example.hashpotatoesv20.Profile.AccountSettingsActivity;
+import com.example.hashpotatoesv20.Profile.ProfileActivity;
 import com.example.hashpotatoesv20.R;
 import com.example.hashpotatoesv20.Utils.BottomNavigationViewHelper;
+import com.example.hashpotatoesv20.Utils.FirebaseMethods;
 import com.example.hashpotatoesv20.Utils.SectionsPagerAdapter;
 import com.example.hashpotatoesv20.Utils.UniversalImageLoader;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,9 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
     private Context mContext = MainActivity.this;
 
+    private ImageView createPost;
+
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
 
 
     @Override
@@ -40,11 +56,22 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: starting.");
 
+        //add icon to navigate to CreatePostActivity
+        ImageView createPost = (ImageView) findViewById(R.id.createPost);
+        createPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating back to CreatePostActivity");
+                Intent intent = new Intent(mContext, CreatePostActivity.class);
+                startActivity(intent);
+            }
+        });
+
         setupFirebaseAuth();
 
         initImageLoader();
         setupBottomNavigationView();
-        setupViewPager();
+        //setupViewPager();
     }
 
     private void initImageLoader(){
@@ -52,19 +79,48 @@ public class MainActivity extends AppCompatActivity {
         ImageLoader.getInstance().init(universalImageLoader.getConfig());
     }
 
+    /*
+    @Nullable
+    //@Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container,false);
+
+        //setupFirebaseAuth();
+
+        //back arrow for navigating back to "Profile Activity"
+        ImageView createPost = (ImageView) view.findViewById(R.id.createPost);
+        createPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating back to ProfileActivity");
+                Intent intent = new Intent(mContext, CreatePostFragment.class);
+                startActivity(intent);
+            }
+        });
+
+        setupFirebaseAuth();
+
+        //initImageLoader();
+        setupBottomNavigationView();
+
+        return view;
+    }*/
+
     /**
      * Responsible for adding the top tab: Home
      */
     private void setupViewPager() {
         SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new MainFragment());
+        //adapter.addFragment(new CreatePostFragment());
+
         ViewPager viewPager = (ViewPager) findViewById(R.id.container);
         viewPager.setAdapter(adapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
-        //tabLayout.getTabAt(0).setIcon(R.drawable.ic_potato);
+        //tabLayout.getTabAt(1).setIcon(R.drawable.ic_createpost);
     }
 
     /**
